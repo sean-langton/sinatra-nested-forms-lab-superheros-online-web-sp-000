50ae47abef4348ac4f6d217d61bb5929dcbe7346
Sean Langton <slangton1@gmail.com>
2019-01-31 23:25:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,13 @@ class App < Sinatra::Base
       @team_motto = params[:team][:motto]
       @hero_1_name = params[:team][:heroes][0][:name]
       @hero_1_power = params[:team][:heroes][0][:power]
-      @hero_1_bio = params[:team][:heroes][0][:bio]
+      @hero_1_bio = params[:team][:heroes][0][:biography]
       @hero_2_name = params[:team][:heroes][1][:name]
       @hero_2_power = params[:team][:heroes][1][:power]
-      @hero_2_bio = params[:team][:heroes][1][:bio]
+      @hero_2_bio = params[:team][:heroes][1][:biography]
       @hero_3_name = params[:team][:heroes][2][:name]
       @hero_3_power = params[:team][:heroes][2][:power]
-      @hero_3_bio = params[:team][:heroes][2][:bio]
+      @hero_3_bio = params[:team][:heroes][2][:biography]
 
       erb :super_hero
     end
